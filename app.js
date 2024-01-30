f4471eb94fbc5d0d8f88be6632b974eae0ea174d
Naresh Tak <taknaresh515@gmail.com>
2024-01-30 16:46:48 +0530

    change the layout of 2 page

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ $(document).ready(function () {
       $(scrollTop).removeClass("active");
     }
   });
+
+  var items = $(".pagination_item");
+    var numItems = items.length;
+    var perPage = 9;
+
+    items.slice(perPage).hide();
+
+    $('.pagination').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "",
+        nextText: "&#8250",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
 });
